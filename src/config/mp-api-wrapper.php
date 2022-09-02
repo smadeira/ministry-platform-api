f8289306c994b633783e0b2c3660a97586de7653
Chris Drew <39171327+Cdrew92@users.noreply.github.com>
2022-09-01 19:43:01 -0500

    init config file

---
+++ src/config/mp-api-wrapper.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+
+# Current System Info
+"MP_API_ENDPOINT" => "",
+"MP_OAUTH_DISCOVERY_ENDPOINT" => "",
+"MP_API_SCOPE" => "",
+
+# Data from Ministry Platform API Client
+"MP_CLIENT_ID" => "",
+"MP_CLIENT_SECRET" => "",
+"MP_OAUTH_REDIRECT_URL" => "",
+"MP_ACCOUNT_NAME" => "www",
+)
